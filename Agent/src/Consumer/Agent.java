8e811ea72b7854b07c15043493fff8854d791327
Hezi Debby <hezided@gmail.com>
2018-05-20 14:15:31 +0300

    Merge branch 'master' of C:\Users\Hezi\IdeaProjects\Enigma with conflicts.

--- Agent/src/Consumer/Agent.java
+++ Agent/src/Consumer/Agent.java
@@ -1,30 +1,37 @@
-package Consumer;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 import DataTypes.GeneratedMachineDataTypes.Decipher;
+import DataTypes.GeneratedMachineDataTypes.Dictionary;
 import DataTypes.GeneratedMachineDataTypes.Machine;
 import DataTypes.SecretWithMissionSize;
+import calc.SecretCalc;
 import pukteam.enigma.component.machine.api.Secret;
 import pukteam.enigma.component.machine.builder.EnigmaMachineBuilder;
 import pukteam.enigma.factory.EnigmaComponentFactory;
 
 public class Agent implements Runnable{
+    //private int m_missionSize;
     private BlockingQueue<SecretWithMissionSize> m_missionsQueue;
     private BlockingQueue<String> m_decipheredQueue;
     private String m_stringToProcess;
     private EnigmaMachineWrapper m_machineWrapper;
-
+    private Dictionary m_dictionry;
+    private String m_abc;
     public Agent(BlockingQueue<SecretWithMissionSize> missionsQueue,
                  BlockingQueue<String> decipheredQueue,
+                 int missionSize,
                  String stringToProcess,
                  Machine xmlMachine,
                  Decipher decipher){
-        xmlMachine.getABC().charAt(xmlMachine.getABC().length());//TODO:: check if length or length-1
+
         m_missionsQueue = missionsQueue;
         m_decipheredQueue = decipheredQueue;
+        //m_missionSize = missionSize;
         m_stringToProcess = stringToProcess;
-
+        m_dictionry = decipher.getDictionary();
+        m_abc = xmlMachine.getABC();
         EnigmaMachineBuilder machineBuilder = EnigmaComponentFactory.INSTANCE.buildMachine(xmlMachine.getRotorsCount(),xmlMachine.getABC());
         EnigmaMachineApplication.DefineRotors(machineBuilder,xmlMachine);
         EnigmaMachineApplication.DefineReflectors(machineBuilder,xmlMachine);
@@ -33,9 +40,38 @@ public class Agent implements Runnable{
 
     @Override
     public void run() {
-
+        while(true){
+            if(!Thread.interrupted()){//TODO:: verify that interrupted works
+                try {
+                    RunMission(m_missionsQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-    private void PerformSingleMission(){
+    private void RunMission(SecretWithMissionSize mission){
+        Secret localSecret = mission.getSecret();
 
+        for(int i = 0 ; i < mission.getMissionSize() ; i++){
+            PerformSingleString(localSecret);
+            try {
+                localSecret = SecretCalc.AdvanceSecret(localSecret,m_abc);
+            }catch (Exception e){
+
+            }
+        }
+    }
+    private void PerformSingleString(Secret secret){
+        String[] processedWords;
+        m_machineWrapper.initFromSecret(secret);
+        String processedString = m_machineWrapper.process(m_stringToProcess);
+        processedWords = processedString.split(" ");
+        for(String s : processedWords){
+            if(!m_dictionry.getWords().contains(s)){
+                return;
+            }
+        }
+        m_decipheredQueue.add(processedString);
     }
 }
--- DM/src/calc/SecretCalc.java
+++ DM/src/calc/SecretCalc.java
@@ -7,34 +7,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class SecretCalc {
-    public static Secret addPositions(Integer[] count, Secret m_secret, Integer i_missionSizeSelection, Integer rotorsCount, String i_abc) {
-        List<Integer> positions = m_secret.getSelectedRotorsPositions();
-        Integer pos;
+    public static Secret addPositions(Integer iMissionSizeSelection, Integer i_missionSizeSelection, String abc) {
 
-        for(int i=0; i<i_missionSizeSelection; i++){
-            pos = positions.get(positions.size()-1);
-            pos++;
-            pos = Integer.parseInt(Integer.toString(pos, i_abc.length()));
-            positions.set(positions.size()-1, pos);
-            if(pos == 0){
-                for(int j=positions.size()-2; j>0; j--){
-                    pos = positions.get(j);
-                    pos++;
-                    pos = Integer.parseInt(Integer.toString(pos, i_abc.length()));
-                    positions.set(j, pos);
-                    if(pos != 0)
-                        break;
-                }
-            }
-        }
-        m_secret.setInitialPosition(positions);
-
-        return m_secret;
     }
 
     public static Secret resetRotorsPositions(Secret m_secret, int rotorsCount) {
         List<Integer> newPosition = new ArrayList<>(Collections.nCopies(rotorsCount, 0));
         m_secret.setInitialPosition(newPosition);
         return m_secret;
+    }
+
+    public static Secret AdvanceSecret(Secret secret, String abc) {
+        for(Integer i : secret.getSelectedRotorsPositions()){
+
+    }
     }
 }
