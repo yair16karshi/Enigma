6641986ce00a926dbd89f8860a46d76544c9e8e2
Yair Karshi <yairka@mta.ac.il>
2018-05-20 14:12:30 +0300

    Add DM module

--- DM/src/calc/SecretCalc.java
+++ DM/src/calc/SecretCalc.java
@@ -12,11 +12,24 @@ public class SecretCalc {
         Integer pos;
 
         for(int i=0; i<i_missionSizeSelection; i++){
-            pos = positions.get(positions.size());
+            pos = positions.get(positions.size()-1);
             pos++;
             pos = Integer.parseInt(Integer.toString(pos, i_abc.length()));
-
+            positions.set(positions.size()-1, pos);
+            if(pos == 0){
+                for(int j=positions.size()-2; j>0; j--){
+                    pos = positions.get(j);
+                    pos++;
+                    pos = Integer.parseInt(Integer.toString(pos, i_abc.length()));
+                    positions.set(j, pos);
+                    if(pos != 0)
+                        break;
+                }
+            }
         }
+        m_secret.setInitialPosition(positions);
+
+        return m_secret;
     }
 
     public static Secret resetRotorsPositions(Secret m_secret, int rotorsCount) {
